71226573342f94e03d383932e554b7bd179421e6
daonham <daonham95@gmail.com>
2023-03-02 08:27:08 +0700

    feat: update for next 13.2

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,10 @@
 import './index.css'
 
+export const metadata = {
+  title: 'My App',
+  description: 'My App Description',
+}
+
 export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
     <html lang="en" className="h-full bg-white text-slate-900 antialiased">
